fdb3ff8568eeec2bbad3162a4770bd31c5883bce
AbdurrahmanEshibiny <73793952+AbdurrahmanEshibiny@users.noreply.github.com>
2022-04-20 17:51:27 +0200

    refixed order issue

--- laneDetection.py
+++ laneDetection.py
@@ -246,44 +246,9 @@ class Lane():
         # Fit new polynomials
         self.fit_poly(binary_warped.shape, leftx, lefty, rightx, righty)  
         
-        
-def merge_image(background, overlay, x, y):
-
-    background_width = background.shape[1]
-    background_height = background.shape[0]
-
-    if x >= background_width or y >= background_height:
-        return background
-
-    h, w = overlay.shape[0], overlay.shape[1]
-
-    if x + w > background_width:
-        w = background_width - x
-        overlay = overlay[:, :w]
-
-    if y + h > background_height:
-        h = background_height - y
-        overlay = overlay[:h]
-
-    if overlay.shape[2] < 4:
-        overlay = np.concatenate(
-            [
-                overlay,
-                np.ones((overlay.shape[0], overlay.shape[1], 1), dtype = overlay.dtype) * 255
-            ],
-            axis = 2,
-        )
-
-    overlay_image = overlay[..., :3]
-    mask = overlay[..., 3:] / 255.0
-
-    background[y:y+h, x:x+w] = (1.0 - mask) * background[y:y+h, x:x+w] + mask * overlay_image
-
-    return background
-
-        ##################################################
-def process_image(self, img):
-########### PUT FULL FUNCTION
+    ##################################################
+    def process_image(self, img):
+        ########### PUT FULL FUNCTION
         warped, Minv, undist, w = binary_warper(img)
         if self.detected:
             # if lines are detected in past frame use those for fitting new polynomials
@@ -341,7 +306,42 @@ def process_image(self, img):
         cv2.putText(result,"C. Position: " + "{:0.2f}".format(offset) + 'm', org=(50,150), fontFace=cv2.FONT_HERSHEY_SIMPLEX,
                 fontScale=1, color=(255,255,255), lineType = cv2.LINE_AA, thickness=2)
         return result
-       
+        
+        
+def merge_image(background, overlay, x, y):
+
+    background_width = background.shape[1]
+    background_height = background.shape[0]
+
+    if x >= background_width or y >= background_height:
+        return background
+
+    h, w = overlay.shape[0], overlay.shape[1]
+
+    if x + w > background_width:
+        w = background_width - x
+        overlay = overlay[:, :w]
+
+    if y + h > background_height:
+        h = background_height - y
+        overlay = overlay[:h]
+
+    if overlay.shape[2] < 4:
+        overlay = np.concatenate(
+            [
+                overlay,
+                np.ones((overlay.shape[0], overlay.shape[1], 1), dtype = overlay.dtype) * 255
+            ],
+            axis = 2,
+        )
+
+    overlay_image = overlay[..., :3]
+    mask = overlay[..., 3:] / 255.0
+
+    background[y:y+h, x:x+w] = (1.0 - mask) * background[y:y+h, x:x+w] + mask * overlay_image
+
+    return background
+
 
 def main():
 
