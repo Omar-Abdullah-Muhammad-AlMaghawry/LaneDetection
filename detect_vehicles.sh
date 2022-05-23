41b58f21dcba4d5f34f6907d36efcee743a5814a
Omar Abdullah <omar.amaghawry@gmail.com>
2022-05-24 00:21:59 +0200

    add the training, slider, shell file, merge main, jubiter

---
+++ detect_vehicles.sh
@@ -0,0 +1,14 @@
+pip install opencv-python
+pip install numpy
+pip install glob
+pip install matplotlib
+pip install IPython
+pip install scipy
+pip install multiprocessing
+pip install moviepy
+pip install sklearn
+pip install skimage
+pip install joblib
+
+
+python3 detect_vehicles.py $1 $2 $3
